31cbf97872da11ecb625b7401acb80d8836e00d2
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2015-09-21 10:51:12 -0400

    Feat: Method blueprints

--- riddle-game.py
+++ riddle-game.py
@@ -1,16 +1,32 @@
 class Engine(object):
 
-    def move(self):
-        print "I will move now"
+    def __init__(self, a_map):
+        self.map = a_map
 
 class Inventory(object):
-    pass
+
+    keys = []
+
+    def show(self):
+        # show which keys are currently posssesed
+        pass
+
+    def add(self):
+        # add a key to the inventory
+        pass
 
 class Room(object):
-    pass
+
+    def inventory(self):
+        # somehow access inventory
+        pass
+
+    def move(self):
+        # move in various directions using Map
+        pass
 
 class Map(object):
     pass
 
-game = Engine()
-game.move()
+the_map = Map()
+game = Engine(the_map)
