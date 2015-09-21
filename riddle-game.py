1b1d03687275e5cc471a63a3195c6f825341b8ac
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2015-09-21 10:43:59 -0400

    Feat: Add boilerplate

---
+++ riddle-game.py
@@ -0,0 +1,16 @@
+class Engine(object):
+
+    def move(self):
+        print "I will move now"
+
+class Inventory(object):
+    pass
+
+class Room(object):
+    pass
+
+class Map(object):
+    pass
+
+game = Engine()
+game.move()
